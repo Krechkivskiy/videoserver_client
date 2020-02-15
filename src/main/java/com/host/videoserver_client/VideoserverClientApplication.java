f8b9211022e6dfc55891dc4d658267a6ecb4cc18
svistylin <vitalik251781@gmail.com>
2020-02-15 09:09:26 +0200

    Initial commit

---
+++ src/main/java/com/host/videoserver_client/VideoserverClientApplication.java
@@ -0,0 +1,13 @@
+package com.host.videoserver_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoserverClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoserverClientApplication.class, args);
+    }
+
+}
