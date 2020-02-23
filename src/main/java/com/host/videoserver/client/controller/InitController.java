685eba1c149c9a7277f28f1c74455a6b4ccaaa8a
svistylin <vitalik251781@gmail.com>
2020-02-24 00:55:49 +0200

    removed

--- src/main/java/com/host/videoserver/client/controller/InitController.java
+++ src/main/java/com/host/videoserver/client/controller/InitController.java
@@ -8,6 +8,7 @@ import com.host.videoserver.client.service.VideoService;
 import com.host.videoserver.client.util.HttpHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,20 +29,22 @@ public class InitController {
     private final UserService userService;
     private final HttpHandler requestHandler;
     private final VideoService videoService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public InitController(UserRepository userRepository, UserService userService,
-                          HttpHandler requestHandler, VideoService videoService) {
+    public InitController(UserRepository userRepository, UserService userService, HttpHandler requestHandler,
+                          VideoService videoService, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userService = userService;
         this.requestHandler = requestHandler;
         this.videoService = videoService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
     public void init() {
         User admin = new User();
         admin.setEmail(defaultServerUsername);
-        admin.setPassword(defaultServerPassword);
+        admin.setPassword(passwordEncoder.encode(defaultServerPassword));
         admin.setSurname("misha");
         admin.setLastname("krechkivskiy");
         String token = requestHandler.registerUserAuthRequest(defaultServerUsername, defaultServerPassword);
